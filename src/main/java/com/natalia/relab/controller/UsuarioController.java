4c41df17ebb196a1d7dc9ce1cc9cec8f4fe3d80d
ngarre <a28710@svalero.com>
2025-10-24 17:00:48 +0200

    hago operacion listar todos usuarios en capa controller

--- src/main/java/com/natalia/relab/controller/UsuarioController.java
+++ src/main/java/com/natalia/relab/controller/UsuarioController.java
@@ -1,6 +1,9 @@
 package com.natalia.relab.controller;
 
 import com.natalia.relab.model.Usuario;
+import com.natalia.relab.repository.UsuarioRepository;
+import com.natalia.relab.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,13 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/usuarios")
     public List<Usuario> listarTodos() {
-        return null;
+        List<Usuario> todosUsuarios = usuarioService.listarTodos();
+        return todosUsuarios;
     }
 
     @PostMapping("/usuarios")
