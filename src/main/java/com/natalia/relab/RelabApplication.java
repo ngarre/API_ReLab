b912a77a8e5330c6dfc22cfb7731ae9a04a7fffd
ngarre <a28710@svalero.com>
2025-10-24 12:23:11 +0200

    Esqueleto del proyecto creado con Initializr

---
+++ src/main/java/com/natalia/relab/RelabApplication.java
@@ -0,0 +1,13 @@
+package com.natalia.relab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natalia/relab/RelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natalia.relab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
