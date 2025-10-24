4ea7536ecbc87ab794dada6e699debfbab3eb152
ngarre <a28710@svalero.com>
2025-10-24 16:53:53 +0200

    metodo para listar todos los usuarios

--- src/main/java/com/natalia/relab/repository/UsuarioRepository.java
+++ src/main/java/com/natalia/relab/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package com.natalia.relab.repository;
 import com.natalia.relab.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
+
+    List<Usuario> findAll();
 }
--- src/main/java/com/natalia/relab/service/UsuarioService.java
+++ src/main/java/com/natalia/relab/service/UsuarioService.java
@@ -1,17 +1,25 @@
 package com.natalia.relab.service;
 
 import com.natalia.relab.model.Usuario;
+import com.natalia.relab.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UsuarioService {
+
+    @Autowired                                     // Así hacemos que la capa Service pueda comunicarse con la Repository.  Crea una instancia de la clase en repository cada vez que llame a metodos de la capa service
+    private UsuarioRepository usuarioRepository;
 
     public void agregar(Usuario usuario) {}
 
     public void eliminar(Usuario usuario) {}
 
     public List<Usuario> listarTodos() {
-        return null;
+        List<Usuario> todosUsuarios = usuarioRepository.findAll();
+        return todosUsuarios;
     }
 
     public Usuario buscarPorId(long id) {
